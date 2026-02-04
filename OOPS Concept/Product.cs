4b0266285c1c2367ef7df64e8ae4b5657d9f2dc7
vipul823 <vipul.sharma823@gmail.com>
2026-02-04 15:47:32 +0530

    Add project files.

---
+++ OOPS Concept/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOPS_Concept
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+
+        public Product(string name, decimal price, string category)
+        {
+            Name = name;
+            Price = price;
+            Category = category;
+        }
+
+        public static Dictionary<string, List<Product>> GroupProductsByCategory(List<Product> products)
+        {
+            Dictionary<string, List<Product>> groupedData = new Dictionary<string, List<Product>>();
+
+            if (products == null)
+                return groupedData;
+
+            foreach (Product product in products)
+            {
+                if (!groupedData.ContainsKey(product.Category))
+                {
+                    groupedData[product.Category] = new List<Product>();
+                }
+
+                groupedData[product.Category].Add(product);
+            }
+
+            return groupedData;
+        }
+    }
+}
---
+++ OOPS Concept/ProductTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace OOPS_Concept
+{
+    [TestFixture]
+    public class ProductTests
+    {
+        [Test]
+        public void TestGroupByCategory()
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product("Laptop", 500, "Electronics"),
+                new Product("Phone", 95, "Electronics"),
+                new Product("Shirt", 180, "Clothing")
+            };
+
+            Dictionary<string, List<Product>> groupedProducts = Product.GroupProductsByCategory(products);
+
+            Assert.That(groupedProducts.Count, Is.EqualTo(2));
+            Assert.That(groupedProducts.ContainsKey("Clothing"), Is.True);
+            Assert.That(groupedProducts["Electronics"].Count, Is.EqualTo(2));
+        }
+    }
+}
